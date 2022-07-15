89119bd046587c9ca58b09919b2ab806ab41d77b
Amine-kun <aminehlab@gmail.com>
2022-07-15 14:50:51 +0200

    adding db

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const Clarifai=require('clarifai');
 const db = knex({
    client: 'pg',
    connection:{
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'test',
-    database: 'smartbrain'     
+    connectionString: process.env.DATABASE_URL,
+    SSL: true,  
    }
 })
 
